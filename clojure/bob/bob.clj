0fae2b8969aeb5317ebf8089df3f5001380cd303
Jake McCrary <jake@jakemccrary.com>
2013-09-11 11:48:45 -0500

    first exercise

---
+++ clojure/bob/bob.clj
@@ -0,0 +1,13 @@
+(ns bob)
+
+(defn- silence? [phrase] (empty? (.trim phrase)))
+(defn- shouting? [phrase] (and (not (silence? phrase))
+                                 (= (.toUpperCase phrase) phrase)))
+(defn- question? [phrase] (.endsWith phrase "?"))
+
+(defn response-for [phrase]
+  (cond
+   (silence? phrase)  "Fine. Be that way!"
+   (shouting? phrase) "Woah, chill out!"
+   (question? phrase) "Sure."
+   :else              "Whatever."))

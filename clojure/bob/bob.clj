87c2abb7fb9f8c701aaf38dd76f007651cf154fe
Jake McCrary <jake@jakemccrary.com>
2013-09-11 11:49:34 -0500

    spacing

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,9 +1,14 @@
 (ns bob)
 
-(defn- silence? [phrase] (empty? (.trim phrase)))
-(defn- shouting? [phrase] (and (not (silence? phrase))
-                                 (= (.toUpperCase phrase) phrase)))
-(defn- question? [phrase] (.endsWith phrase "?"))
+(defn- silence? [phrase]
+  (empty? (.trim phrase)))
+
+(defn- shouting? [phrase]
+  (and (not (silence? phrase))
+       (= (.toUpperCase phrase) phrase)))
+
+(defn- question? [phrase]
+  (.endsWith phrase "?"))
 
 (defn response-for [phrase]
   (cond

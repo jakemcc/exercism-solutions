4da3bd526f2d01b28f066aa3ce103ca1e55a4ea5
Jake McCrary <jake@jakemccrary.com>
2013-09-13 11:50:09 -0500

    change to use a more powerful regex

--- clojure/word-count/word-count.clj
+++ clojure/word-count/word-count.clj
@@ -1,22 +1,10 @@
 (ns phrase
-  (:refer-clojure :exclude [empty?])
   (:require [clojure.string :as string]))
-
-(defn- empty? [s]
-  (clojure.core/empty? (string/trim s)))
-
-(defn- remove-blank [words]
-  (remove empty? words))
-
-(defn- remove-special-characters [s]
-  (string/replace s #"[^a-z0-9 ]" ""))
 
 (defn- words [phrase]
   (-> phrase
       string/lower-case
-      remove-special-characters
-      (string/split #" ")
-      remove-blank))
+      (string/split #"\W+")))
 
 (defn word-count [phrase]
   (frequencies (words phrase)))

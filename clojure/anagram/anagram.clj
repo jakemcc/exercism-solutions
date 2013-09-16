2131187cbbb4f49716c5a205d5dffdf7c921f81a
Jake McCrary <jake@jakemccrary.com>
2013-09-16 10:28:41 -0500

    Simplified check for anagram.

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -2,19 +2,12 @@
   (:require [clojure.string :as str]))
 
 (defn same-letters? [word other-word]
-    (let [x (frequencies (str/lower-case word))
-        potential (frequencies (str/lower-case other-word))
-        char-occurances-word (map x (keys potential))]
-    (every? identity
-            (concat char-occurances-word
-                    (map #(<= %1 %2)
-                         (vals potential)
-                         char-occurances-word)))))
+  (= (sort (str/lower-case word))
+     (sort (str/lower-case other-word))))
 
 (defn anagram?
   [word potential-anagram]
   (and (not= word potential-anagram)
-       (= (count word) (count potential-anagram))
        (same-letters? word potential-anagram)))
 
 (defn anagrams-for

eddd5da9e6d589a16ba6034207ff32cde4dda969
Jake McCrary <jake@jakemccrary.com>
2013-09-16 09:57:25 -0500

    cleaned up code a little bit

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,20 +1,22 @@
 (ns anagram
   (:require [clojure.string :as str]))
 
-(defn anagram?
-  [word potential-anagram]
-  (let [x (frequencies (str/lower-case word))
-        potential (frequencies (str/lower-case potential-anagram))
+(defn same-letters? [word other-word]
+    (let [x (frequencies (str/lower-case word))
+        potential (frequencies (str/lower-case other-word))
         char-occurances-word (map x (keys potential))]
     (every? identity
-            (concat [(= (count word) (count potential-anagram))
-                     (not= word potential-anagram)]
-                    char-occurances-word
+            (concat char-occurances-word
                     (map #(<= %1 %2)
                          (vals potential)
                          char-occurances-word)))))
 
+(defn anagram?
+  [word potential-anagram]
+  (and (not= word potential-anagram)
+       (= (count word) (count potential-anagram))
+       (same-letters? word potential-anagram)))
+
 (defn anagrams-for
   [word potential-anagrams]
-  (filter (partial anagram? word) potential-anagrams)
-  )
+  (filter (partial anagram? word) potential-anagrams))
